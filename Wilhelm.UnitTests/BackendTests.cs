6535db1d6d34a278aa93b8cfc37dd414eaee58a7
Anastasia <khlebousa@gmail.com>
2017-11-03 11:31:37 +0100

    W-6 TestCase example

--- Wilhelm.UnitTests/BackendTests.cs
+++ Wilhelm.UnitTests/BackendTests.cs
@@ -18,5 +18,14 @@ namespace Wilhelm.UnitTests
             int result = c.Add(2, 3);
             Assert.AreEqual(expectedResult, result);
         }
+
+        [TestCase(5,1,4)]
+        [TestCase(7,3,4)]
+        public void Add_AddPositive_ReturnsNumber(int expected, int a, int b)
+        {
+            Class1 c = new Class1();
+            int result = c.Add(a,b);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
