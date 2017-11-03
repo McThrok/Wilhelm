6ee75e7a9819a898ebf54b9d922572954b3103f3
Anastasia <khlebousa@gmail.com>
2017-11-03 11:28:05 +0100

    W-6 Add UnitTests project, add first unit test

---
+++ Wilhelm.UnitTests/BackendTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wilhelm.Backend;
+
+namespace Wilhelm.UnitTests
+{
+    class BackendTests
+    {
+        [Test]
+        public void Add_AddPositive_Returns_5()
+        {
+            Class1 c = new Class1();
+            int expectedResult = 5;
+            int result = c.Add(2, 3);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
