3ac0862e51562cbb5ea337be83a40d7b962691e5
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2017-11-15 12:34:32 +0100

    W-17 add archive handling

--- Wilhelm.Backend/Services/ActivityGenerationService.cs
+++ Wilhelm.Backend/Services/ActivityGenerationService.cs
@@ -14,24 +14,17 @@ namespace Wilhelm.Backend.Services
         {
             List<WActivity> generatedActivities = new List<WActivity>();
             foreach (var task in tasks)
-            {
-                var lastActivity = activities.LastOrDefault(x => x.WTask.Id == task.Id);
-                generatedActivities.AddRange(GenerateActivitiesForTask(lastActivity, task, date));
-            }
+                if (!task.Archivized)
+                {
+                    var lastActivity = activities.LastOrDefault(x => x.WTask.Id == task.Id);
+                    generatedActivities.AddRange(GenerateActivitiesForTask(lastActivity, task, date));
+                }
 
             return generatedActivities;
         }
         public List<WActivity> GenerateActivitiesForTask(WActivity lastActivity, WTask task, DateTime date)
         {
             var result = new List<WActivity>();
-            //if (lastActivity != null && lastActivity.Date.Date == date.Date)
-            //    return result;
-
-            //if (DateTime.Compare(task.StartDate.Date, date.Date) > 0)
-            //    return result;
-
-            //if (lastActivity != null && DateTime.Compare(lastActivity.Date.AddDays(task.Frequency), date.Date) > 0)
-            //    return result;
 
             DateTime nextActivityDate = task.StartDate.Date;
 
