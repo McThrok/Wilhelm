bb011c844dc814cafb70bc8b4cef9a0cff678cd3
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2017-12-03 15:13:23 +0100

    W-24 add backend.api

---
+++ Wilhelm.Backend.Api/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Wilhelm.Backend.Services.Interfaces;
+using Wilhelm.Backend.Services;
+using Wilhelm.Backend.Model.Dto;
+
+namespace Wilhelm.Backend.Api.Controllers
+{
+    public class ReportController : ApiController
+    {
+        private readonly IReportService _reportService;
+        public ReportController()
+        {
+            _reportService = new ServiceFactory().CreateReportService();
+        }
+
+        public List<ReportDto> GetReports()
+        {
+            return _reportService.GetReports();
+        }
+    }
+}
